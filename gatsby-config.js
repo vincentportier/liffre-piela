8d52dfc0d942f2b88d8d98a2086a1f3c4d9718c0
Vincent Portier <vinz.portier@gmail.com>
2021-01-19 11:00:01 +1100

    siteMetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,10 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: `Liffre-Piela`,
+    siteUrl: `https://www.liffre-piela.netlify.app`,
+    description: `Association d'aide humanitaire et de coopération avec Piela (Burkina Faso)`,
+  },
   plugins: [`gatsby-plugin-netlify-cms`],
 }
